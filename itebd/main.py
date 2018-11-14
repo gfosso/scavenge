2b4d0718874ec4dd878b662e3e997890d3f11a21
Gianluca <gianluca.lagnese@gmail.com>
2018-11-14 16:12:47 +0100

    entanglement and fluctuations

--- itebd/main.py
+++ itebd/main.py
@@ -4,18 +4,28 @@ from rand_unitary import *
 import matplotlib.pyplot as plt
 
 def main(argv):
-    cristo=mps(bond_dimension=200)
-    L=5
-    for j in range(10):
+    cristo=mps(bond_dimension=1000)
+    L=10
+    N=10
+    corr=np.zeros(L)
+    dev=np.zeros(L)
+    for j in range(N):
         U=rand_unitary()
         cristo.product_state()
-        corr=np.zeros(L)
         for i in range(L):
-            corr[i] = cristo.expectation_Sz()
+            x= np.sum(cristo.spectrum()*np.log2(cristo.spectrum()))
+            corr[i] -= x 
+            dev[i] += x*x
             cristo.evol(U)
-        plt.plot(corr)
-    
+
+    plt.plot(dev/N - (corr/N)**2,'bo')
+    plt.plot(corr/N,'r+')
+    t=np.arange(0,L,1)
+    plt.plot(t**(2/3))
+    plt.xscale('log')
+    plt.yscale('log')
     plt.show()
+
 
 
 if __name__ == "__main__":
