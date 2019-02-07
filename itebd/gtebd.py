3b64abb68e00b3c44052e7af399d7f0b3cd0720a
Gianluca Lagnese <glagnese@sissa.it>
2019-02-07 18:40:55 +0100

    few steps for growing time evolving block decimation

--- itebd/gtebd.py
+++ itebd/gtebd.py
@@ -8,6 +8,7 @@ q=2;chi=100;N=100
 
 B=[];s=[]
 
+#Domain wall initial condition, max charge vs min charge
 B.append(np.zeros(q,1,1))
 B[-1][0,0,0]=1
 B.append(np.zeros(q,1,1))
@@ -16,6 +17,12 @@ s.append(np.ones([1]))
 s.append(np.ones([1]))
 
 U=np.reshape(rand_u1_unitary(q),(q,q,q,q))
-
+l=2 #initial length
 for step in range(N):
-
+	
+	for i_bond in range(0,l,2):#only even numbers
+		pass
+	B.insert(0,np.zeros(q,1,1))
+	B.append(np.zeros(q,1,1))
+	B[0][0,0,0]=1 #first and last in max and min charge state
+	B[-1][q,0,0]=1
--- itebd/main.py
+++ itebd/main.py
@@ -6,19 +6,18 @@ import matplotlib.pyplot as plt
 def main(argv):
 	cristo=mps(bond_dimension=200)
 	L=5
-	N=100
+	N=10
 	corr=np.zeros(L)
 	dev=np.zeros(L)
 	for j in range(N):
 		U=rand_unitary(4)
 		cristo.product_state()
-        	for i in range(L):
+		for i in range(L):
 			x=np.sum(cristo.spectrum()*np.log2(cristo.spectrum()))
-            		corr[i] -=x
+			corr[i] -=x
 			dev[i] += x*x
-            		cristo.evol(U)
-
-   	plt.plot(dev/N - (corr/N)**2,'bo')
+			cristo.evol(U)
+	plt.plot(dev/N - (corr/N)**2,'bo')
 	plt.plot(corr/N,'r+')
 	t=np.arange(0,L,1)
 	plt.plot(t**(2/3),label="2/3")
@@ -26,7 +25,7 @@ def main(argv):
 	plt.xscale("log")
 	plt.yscale("log")
 	plt.legend()
-    	plt.show()
+	plt.show()
 
 
 
